d91751fa82232335da19b16e66d13ccc5a242481
Jeffrey Fermon <jfermon22@gmail.com>
2016-01-16 23:15:02 -0700

    First working version

--- hw01/ViewController.swift
+++ hw01/ViewController.swift
@@ -9,7 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    //MARK: properties
+    @IBOutlet var image: UIImageView!
+    @IBOutlet var assignmentText: UIStackView!
+    var delay = 0
+    var labelHideTimer: NSTimer? = nil
+    //MARK: methods
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,32 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func tapDetected(sender: UITapGestureRecognizer) {
+        assignmentText.hidden = !assignmentText.hidden;
+        print("got tap")
+        resetLabelTimer()
+    }
 
+    @IBAction func pinchDetected(sender: UIPinchGestureRecognizer) {
+        print("got pinch");
+        image.transform = CGAffineTransformScale(image.transform, sender.scale, sender.scale)
+        sender.scale = 1
+        resetLabelTimer()
+    }
+    
+    func hideLabel() {
+        if !assignmentText.hidden
+        {
+            assignmentText.hidden = true
+        }
+
+        print("hide label")
+    }
+    
+    func resetLabelTimer()
+    {
+        labelHideTimer?.invalidate()
+         labelHideTimer = NSTimer.scheduledTimerWithTimeInterval(3.0, target: self, selector:"hideLabel", userInfo: nil, repeats: false)
+    }
 }
 
